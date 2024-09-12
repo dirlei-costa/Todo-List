54639fe6cb5b46f43035bbd72f4ba00ff6adf650
dirlei-costa <dirleisc@hotmail.com>
2024-09-12 10:15:30 -0300

    feat: correction in TaskPage margin

--- src/pages/TaskPage.jsx
+++ src/pages/TaskPage.jsx
@@ -9,7 +9,7 @@ function TaskPage() {
 
   return (
     <div className="w-screen h-screen bg-slate-600 p-6">
-      <div className="w-[500px] space-y-4">
+      <div className="w-[500px] mx-auto space-y-4">
         <div className="flex justify-center relative">
           <button
             onClick={() => navigate(-1)}
